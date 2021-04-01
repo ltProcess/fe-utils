5740dd8ac8dc477747c0cebb602b2df12fbf0228
ltProcess <lt41156004@163.com>
2021-04-01 14:13:43 +0800

    fix: 边界

--- asyncPool.js
+++ asyncPool.js
@@ -3,29 +3,36 @@
  function asyncPool(poolLimit, array, iteratorFn) {
   let i = 0;
   const ret = [];
-  const excuting = [];
-  const enqueue = function() {
-    if (array.length === 0) {
-      return Promise.resolve();
-    }
-    const item = array[i++];
-    const p = Promise.resolve().then(() => iteratorFn(item, array));
-
-    ret.push(p);
-    const e = p.then(() => excuting.splice(excuting.indexOf(e), 1));
-    excuting.push(e);
-
-    let r = Promise.resolve();
-    if (excuting.length >= poolLimit) {
-      r = Promise.race(excuting);
-    }
-    return r.then(() => enqueue());
+  const executing = [];
+  const enqueue = function () {
+      // 边界处理，array为空数组
+      if (i === array.length) {
+          return Promise.resolve();
+      }
+      // 每调一次enqueue，初始化一个promise
+      const item = array[i++];
+      const p = Promise.resolve().then(() => iteratorFn(item, array));
+      // 放入promises数组
+      ret.push(p);
+      // promise执行完毕，从executing数组中删除
+      const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+      // 插入executing数字，表示正在执行的promise
+      executing.push(e);
+      // 使用Promise.rece，每当executing数组中promise数量低于poolLimit，就实例化新的promise并执行
+      let r = Promise.resolve();
+      if (executing.length >= poolLimit) {
+          r = Promise.race(executing);
+      }
+      // 递归，直到遍历完array
+      return r.then(() => enqueue());
   };
   return enqueue().then(() => Promise.all(ret));
- }
+}
 
 
 // const timeout = i => new Promise(resolve => setTimeout(() => resolve(i), i));
 // return asyncPool(2, [1000, 5000, 3000, 2000], timeout).then(results => {
 //     ...
 // })
+const timeout = i => new Promise(resolve => setTimeout(() => resolve(i), i));
+asyncPool(2, [1000, 5000, 3000, 2000], timeout);
