55749449495409fc90bb194b295b61a6288bd39a
ltProcess <lt41156004@163.com>
2021-04-18 22:26:55 +0800

    feat: util tree

---
+++ parseTree.js
@@ -0,0 +1,26 @@
+let tree = [
+  {
+    id: '1',
+    title: '节点1',
+    children: [
+      {
+        id: '1-1',
+        title: '节点1-1'
+      },
+      {
+        id: '1-2',
+        title: '节点1-2'
+      }
+    ]
+  },
+  {
+    id: '2',
+    title: '节点2',
+    children: [
+      {
+        id: '2-1',
+        title: '节点2-1'
+      }
+    ]
+  }
+]
