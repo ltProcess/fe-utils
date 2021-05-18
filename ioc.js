de518703c2f6a708ad08ce5425febaf168ec6b97
ltProcess <lt41156004@163.com>
2021-05-18 11:53:19 +0800

    feat: ioc 缓存

--- ioc.js
+++ ioc.js
@@ -17,9 +17,21 @@
 
 class Container {
 
+  cache = {};
+  
+  getName(Module) {
+    return Module.name.toLowerCase();
+  }
+
   get(Module) {
+    // 缓存
+    if (this.cache[this.getName(Module)]) {
+      return this.cache[this.getName(Module)];
+    }
     // 创建对象
     const obj = new Module();
+    // 缓存使用
+    this.cache[this.getName(Module)] = obj;
     // 拿到属性
     const properties = Object.getOwnPropertyNames(obj);
     for(let p of properties) {
