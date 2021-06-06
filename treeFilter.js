d4a432d70c1f39a3bc7c1ef783a281d1e1327d10
ltProcess <lt41156004@163.com>
2021-06-06 16:59:37 +0800

    feat: tree filter

--- treeFilter.js
+++ treeFilter.js
@@ -1,6 +1,10 @@
+const { list, listToTree } = require('./listToTree');
 function treeFilter(tree, callBack) {
-  return tree.map(node => ({ ... ndoe })).filter(node => {
+  return tree.map(node => ({ ... node })).filter(node => {
     node.children = node.children && treeFilter(node.children, callBack);
     return callBack(node) || (node.children && node.children.length);
   });
 }
+
+const amen = treeFilter(listToTree(list), (node) => node.id === '1-3');
+console.log('amen', amen);
