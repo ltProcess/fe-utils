d74a63c0d8baae230f4ddcf54300f72552616f32
ltProcess <lt41156004@163.com>
2021-06-06 16:06:40 +0800

    feat: 循环 treeToList

--- treeToList.js
+++ treeToList.js
@@ -8,5 +8,21 @@ function treeToList(tree, result = [], level = 0) {
   return result;
 }
 
-const amen = treeToList(listToTree(list));
+function treeToList1(tree) {
+  let node, result = tree.map(node => {
+    node.level = 1;
+    return node;
+  });
+  for (let i = 0; i < result.length; i++) {
+    if (!result[i].children) continue;
+    let list = result[i].children.map(node => {
+      node.level = result[i].level + 1;
+      return node;
+    });
+    result.splice(i+1, 0, ...list);
+  }
+  return result;
+}
+
+const amen = treeToList1(listToTree(list));
 console.log('amen', amen);
