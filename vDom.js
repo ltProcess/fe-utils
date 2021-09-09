0f825a6eba9bd67d4fb8e9494e924d65aa134e3e
ltProcess <lt41156004@163.com>
2021-09-09 10:56:55 +0800

    feat: vDom

---
+++ vDom.js
@@ -0,0 +1,51 @@
+const testJson = {
+  tag: 'DIV',
+  attrs:{
+  id:'app'
+  },
+  children: [
+    {
+      tag: 'SPAN',
+      children: [
+        { tag: 'A', children: [] }
+      ]
+    },
+    {
+      tag: 'SPAN',
+      children: [
+        { tag: 'A', children: [] },
+        { tag: 'A', children: [] }
+      ]
+    }
+  ]
+};
+// 把上诉虚拟Dom转化成下方真实Dom
+// <div id="app">
+//   <span>
+//     <a></a>
+//   </span>
+//   <span>
+//     <a></a>
+//     <a></a>
+//   </span>
+// </div>
+
+function _render(vNode) {
+  // 如果是数字类型转化为字符串
+  if (typeof vnode === "number") {
+    vnode = String(vnode);
+  }
+  // 字符串类型直接就是文本节点
+  if (typeof vnode === "string") {
+    return document.createTextNode(vnode);
+  }
+  const dom = document.createElement(vNode.tag);
+  if (vNode.attrs) {
+    Object.keys(vNode.attrs).forEach(key => {
+      const value = vNode.attrs[key];
+      dom.setAttribute(key, value);
+    });
+  }
+  vNode.children.forEach((child) => dom.appendChild(_render(child)));
+  return dom;
+}
