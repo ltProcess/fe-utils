83a0daa58373fa97d1bc8e687d4c38f6cc6b955e
ltProcess <lt41156004@163.com>
2022-09-05 22:02:27 +0800

    feat: redux compose

---
+++ middleware/redux-compose.js
@@ -0,0 +1,10 @@
+function compose(...funcs) {
+  if (funcs.length === 0) return (arg) => arg;
+  if (funcs.length === 1) return funcs[0];
+  //  (...args) => fn1(fn2(fn3(fn4(...args))))
+  return funcs.reduce(
+    (a, b) =>
+      (...args) =>
+        a(b(...args))
+  );
+}
