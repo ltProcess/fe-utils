e3912a834597fb9f766b77281a42f26e4e02edc4
ltProcess <lt41156004@163.com>
2022-09-12 15:15:58 +0800

    feat: hashMap

--- deepClone.js
+++ deepClone.js
@@ -1,21 +1,24 @@
-function deepClone(origin) {
+function deepCloneCycle(origin, hashMap = new WeakMap()) {
   let result = null;
+  if (hashMap.has(origin)) return hashMap.get(origin); // 查缓存字典中是否已有需要克隆的对象，有的话直接返回同一个对象（同一个引用，不用递归无限创建进而导致栈溢出了）
   if (typeof origin === "object" && origin !== null) {
     // 【类型判断】引用类型，进行递归拷贝（用typeof判断类型要剔除null的情况）
     if (Object.prototype.toString.call(origin) === "[object Array]") {
       // 【类型判断】数组类型，创建一个新数组
       result = [];
+      hashMap.set(origin, result); // 哈希表缓存新值
       // 【遍历赋值】
       origin.forEach((el) => {
-        result.push(deepClone(el)); // 【递归】
+        result.push(deepCloneCycle(el, hashMap)); // 【递归】
       });
     } else {
       // 【类型判断】对象类型，创建一个新对象
       result = {};
+      hashMap.set(origin, result); // 哈希表缓存新值
       for (const key in origin) {
         // 【遍历赋值】对象这里特殊处理了，不遍历拷贝原型链上的属性
         if (origin.hasOwnProperty(key)) {
-          result[key] = deepClone(origin[key]); // 【递归】
+          result[key] = deepCloneCycle(origin[key], hashMap); // 【递归】
         }
       }
     }
